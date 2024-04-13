a2e54496ee09c25f5d7d073aec307dfb0f07014b
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2024-04-13 14:18:47 -0400

    t

--- src/pixart.h
+++ src/pixart.h
@@ -55,10 +55,6 @@ struct pixart_config {
     struct gpio_dt_spec irq_gpio;
     struct spi_dt_spec bus;
     struct gpio_dt_spec cs_gpio;
-    size_t scroll_layers_len;
-    int32_t *scroll_layers;
-    size_t snipe_layers_len;
-    int32_t *snipe_layers;
 };
 
 #ifdef __cplusplus
